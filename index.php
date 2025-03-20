adc73ab634070cc86209e1c506e2bb343468e8d7
Masrur Ahmed Sami <msami@student.fitchburgstate.edu>
2025-03-20 10:02:48 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+<h1>Welcome to the Time Management App</h1>
+<h2>Click here to <a href="login.php">Login</a></h2>
+=======
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
+>>>>>>> Initial commit of TimeMApp
